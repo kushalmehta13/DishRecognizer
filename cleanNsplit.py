5b8e64a149eb8470d12dee4b9c5e1a4e3926d82e
Kushal Mehta <kushalmehta13@gmail.com>
2017-07-06 10:08:46 -0700

    --split_dir and --ratio flags are now added.

--- cleanNsplit.py
+++ cleanNsplit.py
@@ -8,18 +8,26 @@ import argparse
 import imghdr
 from PIL import Image
 
-def createDirectories(Raw_Dataset, isFlagged=False):
+def createDirectories(Raw_Dataset,Split_dataset_path ,isFlagged=False):
+    if Raw_Dataset.split('/')[len(Raw_Dataset.split('/'))-1] == '':
+        pass
+    else:
+        Raw_Dataset = Raw_Dataset + '/'
+    if Split_dataset_path.split('/')[len(Split_dataset_path.split('/'))-1] == '':
+        pass
+    else:
+        Split_dataset_path = Split_dataset_path + '/'
     if isFlagged == False:
         iterPath = iter(os.listdir(Raw_Dataset))
         for RestDir in iterPath:
-            trainNval_root_dir = '../Split_Dataset/' + RestDir
+            trainNval_root_dir = Split_dataset_path + RestDir
             train_dir = trainNval_root_dir + '/Train'
             validation_dir = trainNval_root_dir + '/Validation'
             if not os.path.exists(trainNval_root_dir):
                 os.makedirs(trainNval_root_dir)
                 os.makedirs(train_dir)
                 os.makedirs(validation_dir)
-            for dishes in os.listdir('../Raw_Dataset/' + RestDir):
+            for dishes in os.listdir(Raw_Dataset + RestDir):
                 training_dishes_dirs = train_dir + '/' + dishes
                 validation_dishes_dirs = validation_dir + '/' + dishes
                 if not os.path.exists(training_dishes_dirs) and not os.path.exists(validation_dishes_dirs):
@@ -32,7 +40,7 @@ def createDirectories(Raw_Dataset, isFlagged=False):
             RestDir = l_rawDataSet[len(l_rawDataSet)-2]
         else:
             RestDir = l_rawDataSet[len(l_rawDataSet)-1]
-        trainNval_root_dir = '../Split_Dataset/' + RestDir
+        trainNval_root_dir = Split_dataset_path + RestDir
         if os.path.isdir(trainNval_root_dir):
             shutil.rmtree(trainNval_root_dir)
         train_dir = trainNval_root_dir + '/Train'
@@ -57,9 +65,8 @@ def clean(Raw_Dataset, isFlagged):
                 fileType = m.id_filename(f)
                 if 'text' in fileType:
                     os.remove(f)
-                if 'png' in fileType:
+                if 'png' in fileType or 'PNG' in fileType:
                     try:
-                        print(f,type(f))
                         im=Image.open(f)
                         im.convert('RGB').save(f + ".jpg")
                         im.close()
@@ -70,10 +77,14 @@ def clean(Raw_Dataset, isFlagged):
                 os.rename(f, f + '.jpg')
 
 
-def splitData(Raw_Dataset,isFlagged=False, TrainPerc=80.0, ValidationPerc=20.0):
+def splitData(Raw_Dataset,SplitDataDir,isFlagged=False, TrainPerc=80.0, ValidationPerc=20.0):
     trainNval_root_dir = ''
     train_dir = ''
     validation_dir = ''
+    if SplitDataDir.split('/')[len(SplitDataDir.split('/'))-1] == '':
+        pass
+    else:
+        SplitDataDir = SplitDataDir + '/'
     if isFlagged:
         iterpath = iter(os.walk(Raw_Dataset))
         iterpath.next()
@@ -81,7 +92,7 @@ def splitData(Raw_Dataset,isFlagged=False, TrainPerc=80.0, ValidationPerc=20.0):
         for (DishDirPath,DishDirName,DishFile) in iterpath:
             count = 0
             l_dishDirPath = DishDirPath.split('/')
-            trainNval_root_dir = '../Split_Dataset/' + l_dishDirPath[len(l_dishDirPath)-2]
+            trainNval_root_dir = SplitDataDir + l_dishDirPath[len(l_dishDirPath)-2]
             train_dir = trainNval_root_dir + '/Train'
             validation_dir = trainNval_root_dir + '/Validation'
             total = len(os.listdir(DishDirPath))
@@ -107,7 +118,7 @@ def splitData(Raw_Dataset,isFlagged=False, TrainPerc=80.0, ValidationPerc=20.0):
                 for (DishDirPath,DishDirName,DishFile) in Inneriterpath:
                     count = 0
                     l_dishDirPath = DishDirPath.split('/')
-                    trainNval_root_dir = '../Split_Dataset/' + l_dishDirPath[len(l_dishDirPath)-2]
+                    trainNval_root_dir = SplitDataDir + l_dishDirPath[len(l_dishDirPath)-2]
                     train_dir = trainNval_root_dir + '/Train'
                     validation_dir = trainNval_root_dir + '/Validation'
                     total = len(os.listdir(DishDirPath))
@@ -122,11 +133,26 @@ def splitData(Raw_Dataset,isFlagged=False, TrainPerc=80.0, ValidationPerc=20.0):
                             shutil.copy(dishFilePath, validation_dir + '/' + l_dishDirPath[len(l_dishDirPath)-1])
                         count += 1
 
-def getRawDatasetPath():
+def getFlags():
+    global SplitDataSet, Training_Ratio, Validation_Ratio
     parser = argparse.ArgumentParser()
     parser.add_argument("--which_rest",help="Path to new restaurant directory in Raw dataset")
     parser.add_argument("--raw_dir",help="Path to Raw dataset directory")
+    parser.add_argument("--split_dir",help="Path to split dataset directory")
+    parser.add_argument("--ratio",help="The ratio between training and validation set. Default is 80:20")
     args = parser.parse_args()
+    if args.ratio:
+        Training_Ratio = float(args.ratio.split(',')[0])
+        Validation_Ratio = float(args.ratio.split(',')[1])
+        if Training_Ratio+Validation_Ratio != 100.0:
+            sys.exit("Error: The ratio does not add upto 100. Please enter the correct ratio again.")
+    else:
+        Training_Ratio = 80.0
+        Validation_Ratio = 20.0
+    if args.split_dir:
+        SplitDataSet = args.split_dir
+    else:
+        SplitDataSet = '../Split_Dataset/'
     if args.raw_dir:
         if args.which_rest:
             print('true')
@@ -149,9 +175,10 @@ def getRawDatasetPath():
 def main():
     # if WHICH_REST specified, use Raw_Dataset ='../Raw_Dataset/restaurantName' and give flag as 1
     # If WHICH_REST not specified, it will delete split data and populate all over again
-    (Raw_Dataset_Path,isFlagged) = getRawDatasetPath()
-    createDirectories(Raw_Dataset_Path, isFlagged)
+    (Raw_Dataset_Path,isFlagged) = getFlags()
+    split_dir_path = SplitDataSet
+    createDirectories(Raw_Dataset_Path,split_dir_path ,isFlagged)
     clean(Raw_Dataset_Path, isFlagged)
-    splitData(Raw_Dataset_Path,isFlagged, 80, 20)
+    splitData(Raw_Dataset_Path,split_dir_path,isFlagged, Training_Ratio, Validation_Ratio)
 
 main()
