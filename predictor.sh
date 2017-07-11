50799503e07064fd479f2ec770f9f3f9c3c73d5f
kushal <kushalmehta13@gmail.com>
2017-07-11 14:56:53 -0700

    More path bug fixes

--- predictor.sh
+++ predictor.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 #!/usr/bin/env python
-#$1 is the path to
+#$1 is the path to the image
 pathToImage=$1
 pathToRestModel=$2
-#path where this file Exists
-workingFile=$(find /home/ -name "predictor.sh")
-workingDir=$(dirname workingFile)
 #This internally calls restaurantSelector.py
 restaurant=$(python restaurantExtractor.py --imagePath="$pathToImage")
 OuterModel='Outer.pb'
 OuterLabel='Outer.txt'
-cd "$pathToRestModel"'/'"$restaurant"
-pathToOuterModel=$(find . -name  $OuterModel)
-pathToOuterLabel=$(find . -name  $OuterLabel)
-CategoryPred=$(python $workingDir/predictor.py --imagePath=$pathToImage --modelFullPath=$pathToOuterModel --labelsFullPath=$pathToOuterLabel | tail -5)
+pathToOuterModel=$(find "${pathToRestModel}""${restaurant}" -name  $OuterModel)
+pathToOuterLabel=$(find "${pathToRestModel}${restaurant}"  -name  $OuterLabel)
+CategoryPred=$(python predictor.py --imagePath=$pathToImage --modelFullPath="$pathToOuterModel" --labelsFullPath="$pathToOuterLabel" | tail -5)
 echo "The top 5 Categories are:"
 echo "$CategoryPred"
 mapfile -t top5Cat <<< "$CategoryPred"
 topCat=$(echo "$top5Cat" | cut -f1 -d"(")
-pathToInnerModel=$(find . -name "$topCat.pb")
-pathToInnerLabel=$(find . -name "$topCat.txt")
-dishPred=$(python $workingDir/predictor.py --imagePath=$pathToImage --modelFullPath=$pathToInnerModel --labelsFullPath=$pathToInnerLabel | tail -5)
+pathToInnerModel=$(find "${pathToRestModel}""${restaurant}" -name "$topCat.pb")
+pathToInnerLabel=$(find "${pathToRestModel}""${restaurant}" -name "$topCat.txt")
+dishPred=$(python predictor.py --imagePath=$pathToImage --modelFullPath=$pathToInnerModel --labelsFullPath=$pathToInnerLabel | tail -5)
 mapfile -t top5Dish <<< "$dishPred"
 echo "The dish is: $top5Dish"
