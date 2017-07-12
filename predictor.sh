564fec98641a5c0e12347a44bf5de57822c94275
kushal <kushalmehta13@gmail.com>
2017-07-11 18:14:47 -0700

    mapped predictions to acutal pb files for prediction and handled the case if the category is itself the dish

--- predictor.sh
+++ predictor.sh
@@ -7,15 +7,25 @@ pathToRestModel=$2
 restaurant=$(python restaurantExtractor.py --imagePath="$pathToImage")
 OuterModel='Outer.pb'
 OuterLabel='Outer.txt'
-pathToOuterModel=$(find "${pathToRestModel}""${restaurant}" -name  $OuterModel)
+pathToOuterModel=$(find "${pathToRestModel}${restaurant}" -name  $OuterModel)
 pathToOuterLabel=$(find "${pathToRestModel}${restaurant}"  -name  $OuterLabel)
 CategoryPred=$(python predictor.py --imagePath=$pathToImage --modelFullPath="$pathToOuterModel" --labelsFullPath="$pathToOuterLabel" | tail -5)
 echo "The top 5 Categories are:"
 echo "$CategoryPred"
+echo "$CategoryPred" > categoryPredictions.txt
 mapfile -t top5Cat <<< "$CategoryPred"
-topCat=$(echo "$top5Cat" | cut -f1 -d"(")
-pathToInnerModel=$(find "${pathToRestModel}""${restaurant}" -name "$topCat.pb")
-pathToInnerLabel=$(find "${pathToRestModel}""${restaurant}" -name "$topCat.txt")
-dishPred=$(python predictor.py --imagePath=$pathToImage --modelFullPath=$pathToInnerModel --labelsFullPath=$pathToInnerLabel | tail -5)
-mapfile -t top5Dish <<< "$dishPred"
-echo "The dish is: $top5Dish"
+topCat=$(echo "$top5Cat" | cut -f1 -d"(" | rev | cut -d" " -f2-| rev )
+echo "${pathToRestModel}${restaurant}/$topCat.pb"
+if [ -e "${pathToRestModel}${restaurant}/$topCat.pb" ]; then
+	pathToInnerModel=$(find "${pathToRestModel}${restaurant}" -name "$topCat.pb")
+	pathToInnerLabel=$(find "${pathToRestModel}${restaurant}" -name "$topCat.txt")
+	echo "$pathToInnerModel"
+	echo "$pathToInnerLabel"
+	dishPred=$(python predictor.py --imagePath=$pathToImage --modelFullPath="$pathToInnerModel" --labelsFullPath="$pathToInnerLabel" | tail -5)
+	echo "$dishPred" > dishPredictions.txt
+	mapfile -t top5Dish <<< "$dishPred"
+	topDish=$(echo "$top5Dish" | cut -f1 -d"(" | rev | cut -d" " -f2-| rev )
+	echo "$restaurant -> $topDish" > prediction.txt
+else
+	echo "$restaurant -> $topCat" > prediction.txt
+fi
